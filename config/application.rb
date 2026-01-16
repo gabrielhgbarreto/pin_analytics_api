ba38845f745f5a793b87a46ef6cac5d86863c506
Gabriel Barreto <gabriel.barreto@sptech.school>
2026-01-15 22:56:44 -0300

    Add: solid queue setup

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_job.queue_adapter = :solid_queue
+    config.solid_queue.connects_to = { database: { writing: :queue } }
   end
 end
